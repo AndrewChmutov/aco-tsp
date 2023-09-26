cc76c04341cfb975b5726f5d5dae1149d81385af
Andrew Chmutov <ulman.andre@gmail.com>
2023-09-26 23:09:06 +0200

    Base application

---
+++ include/point.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+struct Point {
+    double x = 0.0, y = 0.0;
+};
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// C++ standard libraries
+#include <iostream>
+
+// Custom libraries
+#include "point.hpp"
+
+int main() {
+    std::cout << "hello world" << std:: endl;
+
+    return 0;
+}
